2768ab032f81b495a7f59830a80cab45e1f75c1d
THAERHABEEB <ahudeisjsjw@gmail.com>
2025-10-06 15:50:07 +0300

    Update Top.py

--- Top.py
+++ Top.py
@@ -78,7 +78,8 @@ if not os.path.exists("students"):
 
 # ===================== 🧠 واجهة المستخدم =====================
 st.markdown("<div class='pulse-animation'></div>", unsafe_allow_html=True)
-st.title("HITU<br>🎓 نظام تسجيل الحضور الذكي")
+st.title("HITU
+🎓 نظام تسجيل الحضور الذكي")
 st.markdown("---")
 
 name = st.text_input("👤 أدخل اسم الطالب:")
@@ -131,6 +132,7 @@ if btn:
     except Exception as e:
 
         st.error(f"حدث خطأ أثناء الحذف: {e}")
+
 
 
 
